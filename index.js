fd94b56a45df7dc80c01d9b7011e63a4e93ba02b
Mark Davies <markmuiruri11@gmail.com>
2025-06-22 00:03:00 +0300

    JavaScript Looping and Iteration Codealong

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+  if (!names || !Array.isArray(names)) {
+    return [];
+  }
+  const thankYouMessages = names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+
+  return thankYouMessages;
+}
+function countDown(startNumber) {
+  for (let i = startNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
